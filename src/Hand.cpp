d003fb34e80b1198b4f86b32ebb8f30681e81342
CatchingSeven <maxheinrich113@gmail.com>
2025-11-15 12:17:54 +1030

    type(feat): finished defining hand class functions. Cant test until player class is defined so i'll do that one next

--- src/Hand.cpp
+++ src/Hand.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "../include/Hand.h"
+
+
+void Hand::addCard(const Card &card) {
+    hand.push_back(card);
+}
+
+int Hand::getBestTotal() const {
+    int aces = 0;
+    int score = 0;
+    for (int i = 0; i < hand.size(); i++) {
+
+        if (hand[i].getValue() == 11) {
+            aces++;
+        }
+        score += hand[i].getValue();
+
+
+    }
+    if (score > 21&& aces > 0) {
+        while (aces > 0) {
+            score -= 10;
+            aces--;
+        }
+    }
+
+    return score;
+}
+
+
+bool Hand::isBust() const{
+    return getBestTotal() > 21;
+}
+
+bool Hand::isBlackjack() const {
+    return getBestTotal() == 21;
+}
+
+std::string Hand::toString() const {
+    std::string output = "";
+    for (int i = 0; i < hand.size(); i++) {
+        output += hand[i].getName() + ", ";
+    }
+
+    return output;
+
+}
+
